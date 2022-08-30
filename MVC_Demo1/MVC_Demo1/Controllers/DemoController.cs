6a5652e56a2142e54c2ccb618ad3a637d5bd34a2
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2022-08-30 17:43:33 +0700

    them demo mvc cs

---
+++ MVC_Demo1/MVC_Demo1/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo1.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Noti = "Hello Duc Anh";
+            return View();
+        }
+    }
+}
