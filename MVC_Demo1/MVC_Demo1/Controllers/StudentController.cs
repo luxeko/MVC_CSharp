4785602d47d91dce72eb1bb66bbc91bcc9e15766
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2022-08-30 17:54:42 +0700

    studen mvc

---
+++ MVC_Demo1/MVC_Demo1/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Demo1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["name"] = "Lê Khả Đức Anh";
+            TempData["age"] = "22";
+            TempData["address"] = "Hà Nội";
+            return View("ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
